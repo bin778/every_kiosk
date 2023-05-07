609c0ae60c73f41e436702b6a4aa0fa8c5f9bed3
최영빈 <bin778@naver.com>
2023-05-07 23:51:44 +0900

    git fixed file

--- frontend/src/view/IngredientSelect.jsx
+++ frontend/src/view/IngredientSelect.jsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import "../css/Modal.css"
 
+import IMG_CHEESE from "../images/cheese.png";
+
 const IngredientSelect = (props) => {
     // 열기, 닫기 텍스트를 부모로부터 받아옴
     const {open, close} = props;
@@ -25,9 +27,13 @@ const IngredientSelect = (props) => {
         <div className={open ? 'openModal modal' : 'modal'}>
             <div className='modalBox modalSelect'>
                 <div className='SelectTitle'>원하는 재료를 선택해주세요</div>
-                <div className='SelectDisplay'>
-                    
-                </div>
+                <ul className='SelectDisplay'>
+                    <li className='SelectChangeMenu'>
+                        <img src={IMG_CHEESE} alt="" />
+                        <span>+200원</span>
+                        <span>치즈</span>
+                    </li>
+                </ul>
                 <span className='modal-button cancel-button bottom left' onClick={close}>취소</span>
                 <span className='modal-button bottom right'>선택</span>
             </div>
