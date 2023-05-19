adaf17bb36e701d34bd0e5fe2e49eb4365396d15
최영빈 <bin778@naver.com>
2023-05-19 23:23:31 +0900

    Update Order.jsx

--- frontend/src/view/Order.jsx
+++ frontend/src/view/Order.jsx
@@ -282,14 +282,14 @@ export default function Order(props) {
               <div className="card-text2 position-up">1개</div>
               <div className="card-text2 red">5,000원</div>
             </li>
-            {/* <li className="order-card">
+            <li className="order-card">
               <div className="card-text1">불고기버거</div>
               <img src={IMG_MENU1} className="ordered" alt="" />
               <img src={IMG_CLOSE} className="btn-close" alt="" />
               <div className="card-text2 position-up">1개</div>
               <div className="card-text2 red">5,000원</div>
             </li>
-            <li className="order-card">
+            {/* <li className="order-card">
               <div className="card-text1">불고기버거</div>
               <img src={IMG_MENU1} className="ordered" alt="" />
               <img src={IMG_CLOSE} className="btn-close" alt="" />
