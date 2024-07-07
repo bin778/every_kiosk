5d81e8ced36ac6b62c1290557c8402c01f65aa32
최영빈 <bin778@naver.com>
2024-07-07 14:42:55 +0900

    fix db

--- backend/src/api/db.ts
+++ backend/src/api/db.ts
@@ -1,39 +1,29 @@
-import mysql, { Pool, QueryResult } from 'mysql2';
+import mysql from 'mysql2';  // mysql 모듈 로드
 
-const conn = {
-  host: '127.0.0.1',
-  port: 3306,
-  user: 'everykiosk',
-  password: '1234',
-  database: 'everykiosk'
+const conn: mysql.ConnectionOptions = {  // mysql 접속 설정
+  host: '127.0.0.1', // DB 호스트 설정
+  port: 3306, // DB 포트번호 설정
+  user: 'everykiosk', // DB 유저 설정
+  password: '1234', // DB 비밀번호 설정
+  database: 'everykiosk' // DB 스키마 설정
 };
 
-const pool: Pool = mysql.createPool(conn);
+const pool = mysql.createPool(conn);
 
-interface DbInterface {
-  TestDB: () => Promise<unknown>;
-}
+const db: { [key: string]: () => Promise<unknown> } = {};
 
-const db: DbInterface = {
-  TestDB: async () => {
-    const sql = `SELECT * FROM test`;
-  
-    try {
-      const results = await new Promise<QueryResult>((resolve, reject) => {
-        pool.query(sql, (err, results) => {
-          if (err) {
-            reject(err);
-          } else {
-            resolve(results);
-          }
-        });
-      });
-      
-      return results;
-    } catch (error) {
-      throw error;
-    }
-  }
+db.TestDB = () => {
+  return new Promise(async (resolve, reject) => {
+    const sql = 'SELECT * FROM test'; // SQL 쿼리
+
+    pool.query(sql, (err, results) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(results);
+      }
+    });
+  });
 };
 
 export default db;
